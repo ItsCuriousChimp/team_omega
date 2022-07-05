b6c352f4d6d82d6201285c87dcc57776e8aa5af5
manish1307caw <107033538+manish1307caw@users.noreply.github.com>
2022-07-05 11:40:58 +0530

    changes made #5

--- book_my_show/heartbeat/services/heartbeat_service.py
+++ book_my_show/heartbeat/services/heartbeat_service.py
@@ -3,7 +3,6 @@ from ..repositories.heartbeat_repository import HeartbeatRepository
 
 
 class HeartbeatService:
-
     def get_last_heartbeat(self) -> datetime:
         call_repository = HeartbeatRepository().fetch_heart_beat()
         return call_repository.last_beat
