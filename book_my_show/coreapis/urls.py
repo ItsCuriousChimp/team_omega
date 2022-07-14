8a79f92894325ab4592f96a2af7950aa31287e61
shreyCreator <shrey.agrawal@cawstudios.com>
2022-07-14 16:41:26 +0530

    merge conflict resolved

--- book_my_show/coreapis/urls.py
+++ book_my_show/coreapis/urls.py
@@ -6,8 +6,7 @@ from book_my_show.coreapis.views.seats_view import SeatView
 
 
 urlpatterns = [
-    path("showtimes/<str:id>/", SeatView.as_view()),
-    path("movies/<str:pk1>/", CinemaView.as_view()),
+    path("showtimes/<str:id>/seats/", SeatView.as_view()),
     path("city/<str:id>/movies/", MovieList.as_view()),
     path("movies/<str:id>/cinemas/", CinemaView.as_view()),
     path("showtime/<str:pk1>/seat/<str:pk2>/", BookingView.as_view()),
