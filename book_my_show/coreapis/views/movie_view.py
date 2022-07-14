2348177edcd718930d5cc3989cc6b8270e11a92e
manish1307caw <107033538+manish1307caw@users.noreply.github.com>
2022-07-14 15:54:27 +0530

    some alterations

--- book_my_show/coreapis/views/movie_view.py
+++ book_my_show/coreapis/views/movie_view.py
@@ -2,15 +2,10 @@ import json
 from django.http import JsonResponse
 from rest_framework.views import APIView
 from book_my_show.coreapis.services.movie_service import MovieService
-from rest_framework.permissions import IsAuthenticated
-from rest_framework.authentication import TokenAuthentication
 
 class MovieList(APIView):
 
     # GET /v1/coreapis/city/<str:id>/movies
-
-    authentication_classes = [TokenAuthentication]
-    permission_classes = [IsAuthenticated]
 
     movie_service = MovieService()
     def get(self, request, id: str) -> JsonResponse:
