eb375417c47ce235e0f8c801860a87a6375d1d0e
manish1307caw <107033538+manish1307caw@users.noreply.github.com>
2022-07-14 14:37:15 +0530

    gave the return types for all

--- book_my_show/coreapis/repositories/movie_repository.py
+++ book_my_show/coreapis/repositories/movie_repository.py
@@ -4,13 +4,7 @@ from book_my_show.coreapis.models.showtime_model import Showtime
 
 
 class MovieRepository:
-    def get_cinemas(self, city_pk) ->Showtime:
+    def get_cinemas(self, city_pk: str) ->Showtime:
         movie_in_city = Showtime.objects.filter(cinema_screen_id__cinema_id_id__city_id_id__id= int(city_pk))
         return movie_in_city
 
-    def get_city_name(self, city_pk) -> str:
-        city = City.objects.all().filter(id = int(city_pk))
-        city_name = "NULL"
-        if city:
-            city_name = str(city[0])
-        return str(city_name)
--- book_my_show/coreapis/services/movie_service.py
+++ book_my_show/coreapis/services/movie_service.py
@@ -1,12 +1,14 @@
 from book_my_show.coreapis.repositories.movie_repository import MovieRepository
 class MovieService:
     cinema_repository = MovieRepository()
-    def get_movies_list(self, city_pk) -> list:
+    def get_movies_list(self, city_pk:str) -> list:
         
         movies_in_city = self.cinema_repository.get_cinemas(city_pk)
-        ls = {}
+        ls = []
         for i in movies_in_city:
-            ls[str(i.movie_id.id)] = str(i.movie_id)
+            movie_detail = {}
+            movie_detail["Movie_Id"] = str(i.movie_id.id)
+            movie_detail["Movie_Name"] = str(i.movie_id)
+            ls.append(movie_detail)
+
         return ls
-    # def get_city_name(self, city_pk) ->str:
-    #     return str(self.cinema_repository.get_city_name(city_pk))
