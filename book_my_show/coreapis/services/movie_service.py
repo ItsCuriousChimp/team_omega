ca8e9c6c4e3886f3e89babeb67cf68266437bd8c
manish1307caw <107033538+manish1307caw@users.noreply.github.com>
2022-07-14 12:42:47 +0530

    change var naming

--- book_my_show/coreapis/services/movie_service.py
+++ book_my_show/coreapis/services/movie_service.py
@@ -1,6 +1,6 @@
 from book_my_show.coreapis.repositories.movie_repository import MovieRepository
 class MovieService:
-    def get_movies(self, city_pk):
+    def get_movies_list(self, city_pk):
         cinema_repository = MovieRepository()
         movies_in_city = cinema_repository.get_cinemas(city_pk)
         ls = []
--- book_my_show/coreapis/views/movie_view.py
+++ book_my_show/coreapis/views/movie_view.py
@@ -5,10 +5,10 @@ from book_my_show.coreapis.services.movie_service import MovieService
 class MovieList(APIView):
 
     def get(self, request,*args, **kwargs):
-        movie_pk = self.kwargs["pk1"]
+        city_pk = self.kwargs["pk1"]
         movie_service = MovieService()
-        ls = movie_service.get_movies(movie_pk)
-        city_name = movie_service.get_city_name(movie_pk)
+        ls = movie_service.get_movies_list(city_pk)
+        city_name = movie_service.get_city_name(city_pk)
         return JsonResponse(
            { city_name: ls}
         )
