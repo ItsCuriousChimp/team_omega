6d6b4632b1ea4f6ed4c0dd8dd04fd73a63884607
TARUSSI <tarussisingh@gmail.com>
2022-07-14 13:24:11 +0530

    added url for api

--- book_my_show/coreapis/urls.py
+++ book_my_show/coreapis/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
 
+from book_my_show.coreapis.views.seat_available_view import SeatAvailableView
+
 
 urlpatterns = [
+    path("showtimes/<str:id>/seats/", SeatAvailableView.as_view()),
 ]
