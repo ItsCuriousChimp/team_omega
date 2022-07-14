3d5be660a46d11d86098b87855417c0c3ba2ca14
TARUSSI <tarussisingh@gmail.com>
2022-07-14 16:12:58 +0530

    added view and service to get seats based on availability

---
+++ book_my_show/coreapis/services/seats_service.py
@@ -0,0 +1,24 @@
+from book_my_show.coreapis.repositories.seats_repository import (
+    SeatRepository,
+)
+
+
+class SeatService:
+    seat_repository = SeatRepository()
+
+    def get_all_seats(self, showtime_pk: str) -> list[dict]:
+        all_seats_of_showtime = self.seat_repository.get_all_seats(showtime_pk)
+        return all_seats_of_showtime
+
+    def get_unavailable_seats(self, showtime_pk: str) -> list[dict]:
+        unavailable_seat_of_showtime = self.seat_repository.get_unavailable_seats(
+            showtime_pk
+        )
+
+        return unavailable_seat_of_showtime
+
+    def get_seat_available(self, showtime_pk: str) -> list[dict]:
+        available_seat_of_showtime = self.seat_repository.get_available_seats(
+            showtime_pk
+        )
+        return available_seat_of_showtime
