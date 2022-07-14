a83a2d243f2678a5739656e1a4bb823ea115f053
TARUSSI <tarussisingh@gmail.com>
2022-07-14 13:48:34 +0530

    added many to many relationship

--- book_my_show/coreapis/models/movie_model.py
+++ book_my_show/coreapis/models/movie_model.py
@@ -1,11 +1,13 @@
 from django.db import models
 from book_my_show.common.models.base_model import BaseModel
+from book_my_show.coreapis.models.cinema_model import Cinema
 
 
 class Movie(BaseModel):
     name = models.CharField(max_length=32)
     description = models.TextField()
     release_date = models.DateField()
+    cinema_id = models.ManyToManyField(Cinema)
 
     def __str__(self):
         return str(self.name)
