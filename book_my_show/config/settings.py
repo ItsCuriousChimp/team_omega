2c262f762e211c3687ef2f89ae491610fdef35d3
shreyCreator <shrey.agrawal@cawstudios.com>
2022-07-22 17:57:30 +0530

    added docker

--- book_my_show/config/settings.py
+++ book_my_show/config/settings.py
@@ -72,7 +72,7 @@ class Settings(Configuration):
             "USER": os.environ.get("DB_USER"),
             "PASSWORD": os.environ.get("DB_PASSWORD"),
             "HOST": os.environ.get("DB_HOST"),
-            "PORT": os.environ.get("DB_PORT"),
+            # "PORT": os.environ.get("DB_PORT"),
         }
     }
 
