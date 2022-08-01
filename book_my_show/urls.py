245da01f8b8c4d30c4d3863376a5791bb3b00895
Tarussi-S <tarussi.singh@cawstudios.com>
2022-08-01 17:16:00 +0530

    updated heartbeat url

--- book_my_show/urls.py
+++ book_my_show/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("v1/heartbeat", include("book_my_show.heartbeat.urls")),
+    path("v1/heartbeat/", include("book_my_show.heartbeat.urls")),
     path("v1/", include("book_my_show.authenticate.urls")),
     path("v1/", include("book_my_show.coreapis.urls")),
 ]
